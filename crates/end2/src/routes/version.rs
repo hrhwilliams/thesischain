c363bc41951fc0f4fd264eb72ed4cc1912c6784c
Harlan Williams <hrw@ku.edu>
2026-03-16 19:25:12 -0500

    refine how git commit gets passed to backend

--- crates/end2/src/routes/version.rs
+++ crates/end2/src/routes/version.rs
@@ -3,6 +3,6 @@ use axum::{Json, response::IntoResponse};
 pub async fn version() -> impl IntoResponse {
     Json(serde_json::json!({
         "version": env!("CARGO_PKG_VERSION"),
-        "commit": env!("GIT_COMMIT"),
+        "commit": std::env::var("GIT_COMMIT").unwrap_or_else(|_| "unknown".to_string()),
     }))
 }
